ab5e50e60bc9424f99c2389e1785ef48bba3a7b4
Ashot Simonyan <ashotsimonyan1991@mail.ru>
2023-05-16 16:01:59 +0400

    fix bug

--- src/main/java/com/example/healthcaremanagement/controller/PatientController.java
+++ src/main/java/com/example/healthcaremanagement/controller/PatientController.java
@@ -3,12 +3,12 @@ package com.example.healthcaremanagement.controller;
 import com.example.healthcaremanagement.entity.Patient;
 import com.example.healthcaremanagement.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -41,13 +41,8 @@ public class PatientController {
     }
 
     @PostMapping("add")
-    public String addPatient(@ModelAttribute Patient patient, @RequestParam("date") String date) throws ParseException {
-        String bDate = "01-01-0001";
-        if (date != "") {
-            patient.setDateOfBirthday(new SimpleDateFormat("yyyy-MM-dd").parse(date));
-        } else {
-            patient.setDateOfBirthday(new SimpleDateFormat("yyyy-MM-dd").parse(bDate));
-        }
+    public String addPatient(@ModelAttribute Patient patient, @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
+        patient.setDateOfBirthday(date);
         patientRepository.save(patient);
         return "redirect:/patients";
     }
